8dcc7e3ffbf47a06058b0b8a7fb0b38eed165342
xprog <bezt.igor@gmail.com>
2021-08-04 09:20:43 +0300

    fix 5.2x

--- exercises/05_basic_scripts/task_5_2a.py
+++ exercises/05_basic_scripts/task_5_2a.py
@@ -42,7 +42,17 @@ bin_ip = "00001010000000010000000111000011"
 
 """
 
-ip = input("введите IP-сети в формате: 10.1.1.0/24\n")
+template = '''Network:
+{0:<8}  {1:<8}  {2:<8}  {3:<8}
+{0:>08b}  {1:>008b}  {2:>08b}  {3:>08b}
+
+
+Mask:
+/{4}
+{5:<8}  {6:<8}  {7:<8}  {8:<8}
+{9:<8}  {10:<8}  {11:<8}  {12:<8}'''
+
+ip = input("введите IP-сети в формате: 10.1.1.0/24: ")
 
 ip_and_mask = ip.split("/")
 ip = ip_and_mask[0]
@@ -51,12 +61,7 @@ mask = ip_and_mask[1]
 ip = ip_and_mask[0]
 mask = ip_and_mask[1]
 
-#ip = "11.22.33.44"
-#mask = "23"
-
 ip = ip.split(".")
-
-#print(ip, mask)
 
 template_mask_0 = "{:<032}"
 mask = int(mask)
@@ -69,28 +74,12 @@ mask2_bin = mask[8:16]
 mask3_bin = mask[16:24]
 mask4_bin = mask[24:]
 
-
-template_ip_dec = "{:<8} {:<8} {:<8} {:<8}"
-template_ip_bin = "{0:>08b} {1:>008b} {2:>08b} {3:>08b}"
-
 ip[0] = int(ip[0]) & int(mask1_bin, 2)
 ip[1] = int(ip[1]) & int(mask2_bin, 2)
 ip[2] = int(ip[2]) & int(mask3_bin, 2)
 ip[3] = int(ip[3]) & int(mask4_bin, 2)
 
-
-
-print("Network:")
-print(template_ip_dec.format(ip[0], ip[1], ip[2], ip[3]))
-print(template_ip_bin.format(int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3])))
-
-template_mask_1 = "\nMask:\n/{}"
-template_mask_bin = "{:>08} {:>08} {:>08} {:>08}"
-template_mask_dec = "{:<8} {:<8} {:<8} {:<8}"
-
-print(template_mask_1.format(mask_dec))
-print(template_mask_dec.format(int(mask1_bin, 2), int(mask2_bin, 2), int(mask3_bin, 2), int(mask4_bin, 2)))
-print(template_mask_bin.format(int(mask1_bin), int(mask2_bin), int(mask3_bin), int(mask4_bin)))
-
-#print("\n", mask)
-#print(mask1_bin, " ", mask2_bin)
+print(template.format(int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3]), \
+                      mask_dec, \
+                      int(mask1_bin, 2), int(mask2_bin, 2), int(mask3_bin, 2), int(mask4_bin, 2), \
+                      mask1_bin, mask2_bin, mask3_bin, mask4_bin))
